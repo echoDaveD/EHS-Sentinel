fd75c8cc2d5399ea84e13810b10e00121b3b3754
echoDaveD <dylong.david@googlemail.com>
2025-02-17 22:07:43 +0100

    asdf

--- MQTTClient.py
+++ MQTTClient.py
@@ -316,8 +316,6 @@ class MQTTClient:
             namenorm = self._normalize_name(nasa)
 
             if nasa not in self.known_topics:
-                entities[namenorm] = "{}"
-            else:
                 sensor_type = "sensor"
                 if 'enum' in self.config.NASA_REPO[nasa]:
                     enum = [*self.config.NASA_REPO[nasa]['enum'].values()]
