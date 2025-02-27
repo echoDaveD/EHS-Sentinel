d95e33bc1308c613b04fb3faf767fe89f1e760ad
echoDaveD <dylong.david@googlemail.com>
2025-02-27 22:44:06 +0100

    mehr logging  für structure

--- NASAPacket.py
+++ NASAPacket.py
@@ -271,7 +271,7 @@ class NASAPacket:
         elif message_type == 2:
             payload_size = 4
         elif message_type == 3:
-            payload_size = len(msg_rest)-3  
+            payload_size = len(msg_rest)  
             print(f"Message {hex(message_number)} paylaod: {msg_rest[2:2 + payload_size]} / {[hex(x) for x in msg_rest[2:2 + payload_size]]}")
             if capacity != 1:
                 raise SkipInvalidPacketException("Message with structure type must have capacity of 1.")
